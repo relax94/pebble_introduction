08a7bc21a8898ac50b2acc4bfcb867887e6cf0d3
Dmitry Pavlenko <basket.pavlenko@gmail.com>
2017-09-03 16:36:45 +0300

    add main window with menu

---
+++ src/c/main_window.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void main_window_create();
+void main_window_destroy();
+Window * main_window_get_window();
--- src/c/splash_window.h
+++ src/c/splash_window.h
@@ -4,4 +4,4 @@ void create_splash_image();
 void create_splash_title();
 void splash_window_create();
 void splash_window_destroy();
-Window * splash_window_get_window();
+Window *splash_window_get_window();
